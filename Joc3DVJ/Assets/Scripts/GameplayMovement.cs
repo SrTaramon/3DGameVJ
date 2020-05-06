f42d6c72400f1d839681b4fab4e92b55d0370b34
SrTaramon <sergitaramon21@gmail.com>
2020-05-06 21:45:27 +0200

    Movement
    Movement of the camera and player comb

---
+++ Joc3DVJ/Assets/Scripts/GameplayMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update(){
+        transform.localPosition += new Vector3(0, 0, 1) * 10 * Time.deltaTime;
+    }
+}
