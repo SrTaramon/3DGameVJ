de55889dddbc4dee16d50ae5fc04eabb3e681446
SrTaramon <sergitaramon21@gmail.com>
2020-05-06 21:38:37 +0200

    MovementImprove
    Rotation on movement applied

--- Joc3DVJ/Assets/Scripts/PlayerMovement.cs
+++ Joc3DVJ/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    private Transform player;
     public float speed;
+
+    void Start(){
+        player = transform.GetChild(0);
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,8 +21,14 @@ public class PlayerMovement : MonoBehaviour
         float horKey = Input.GetAxis("Horizontal");
         float verKey = Input.GetAxis("Vertical");
     
-        if (hor != 0 || ver != 0) AxisMove(hor, ver, speed);
-        if (horKey != 0 || verKey != 0) AxisMove(horKey, verKey, speed);
+        if (hor != 0 || ver != 0){
+            AxisMove(hor, ver, speed);
+            HorizontalLean(player, hor, 50, .1f);
+        } 
+        if (horKey != 0 || verKey != 0){
+            AxisMove(horKey, verKey, speed);
+            HorizontalLean(player, horKey, 50, .1f);
+        } 
 
         if (Input.GetKeyDown(KeyCode.Escape)) SceneManager.LoadScene("Menu", LoadSceneMode.Single);
     }
@@ -34,5 +44,11 @@ public class PlayerMovement : MonoBehaviour
         pos.x = Mathf.Clamp01(pos.x);
         pos.y = Mathf.Clamp01(pos.y);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+     void HorizontalLean(Transform target, float axis, float leanLimit, float lerpTime)
+    {
+        Vector3 targetEulerAngels = target.localEulerAngles;
+        target.localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -axis * leanLimit, lerpTime));
     }
 }
