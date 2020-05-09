70684df719a51d35a732852c1521c70d9b31ee63
SrTaramon <sergitaramon21@gmail.com>
2020-05-09 17:23:13 +0200

    Sounds&Collision
    Sounds effects and collision explosion added

---
+++ Joc3DVJ/Assets/Scripts/SoundManagerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerController : MonoBehaviour
+{
+    public static AudioClip explosion;
+
+    static AudioSource audioSrc;
+
+    void Start(){
+        explosion = Resources.Load<AudioClip>("Explosion");
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(string state){
+        switch (state){
+        case "explosion":
+            audioSrc.PlayOneShot(explosion);
+            break;
+        }
+    }
+}
