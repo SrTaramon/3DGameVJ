a468d5ff870a8bc988dfb1836321ec59ded0621d
oriol.maristany <oriol.maristany@est.fib.upc.edu>
2020-05-12 16:32:12 +0200

    Shooting

---
+++ Joc3DVJ/Assets/Scripts/SpawnProjectil.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectil : MonoBehaviour
+{
+    public GameObject firePoint1;
+    public GameObject firePoint2;
+
+    public GameObject projectil;
+
+    //public GameObject projectilOrientation;
+    //public GameObject projectilOrientation2;
+
+    private Vector3 direction;
+    private Vector3 direction2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        if (Input.GetMouseButtonDown (0)){
+            FSpawnProjectil();
+        }
+        
+    }
+
+    void FSpawnProjectil() {
+        GameObject proj;
+        GameObject proj2;
+
+
+        proj = Instantiate(projectil, firePoint1.transform.position, Quaternion.identity);
+        //direction = projectilOrientation.transform.position - firePoint1.transform.position;
+        //proj.transform.localRotation = Quaternion.LookRotation();
+
+
+        proj2 = Instantiate(projectil, firePoint2.transform.position, Quaternion.identity);
+        //direction2 = projectilOrientation2.transform.position - firePoint2.transform.position;
+        //proj2.transform.localRotation = Quaternion.LookRotation(direction2);
+    }
+}
