2ff828b2c568c091ba96c62e8f722b7b383c6a46
oriol.maristany <oriol.maristany@est.fib.upc.edu>
2020-05-13 20:48:10 +0200

    View + trail

--- Joc3DVJ/Assets/Scripts/PlayerMovement.cs
+++ Joc3DVJ/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,8 @@ public class PlayerMovement : MonoBehaviour
 {
     private Transform player;
     public float speed;
+    public Transform aimTarget;
+    public float lookSpeed = 340;
 
     void Start(){
         player = transform.GetChild(0);
@@ -19,20 +21,30 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float hor = Input.GetAxis("Mouse X");
-        float ver = Input.GetAxis("Mouse Y");
 
-        float horKey = Input.GetAxis("Horizontal");
-        float verKey = Input.GetAxis("Vertical");
-    
+        float hor = Input.GetAxis("Horizontal");
+        if (hor == 0) hor = Input.GetAxis("Mouse X");
+        float ver = Input.GetAxis("Vertical");
+        if (ver == 0) ver = Input.GetAxis("Mouse Y");
+        
+
+        AxisMove(hor, ver, speed);
+        RotationLook(hor,ver, lookSpeed);
+        HorizontalLean(player, hor, 50, .1f);
+
+
+        /*
         if (hor != 0 || ver != 0){
             AxisMove(hor, ver, speed);
+            RotationLook(hor,ver, lookSpeed);
             HorizontalLean(player, hor, 50, .1f);
         } 
         if (horKey != 0 || verKey != 0){
             AxisMove(horKey, verKey, speed);
+            RotationLook(horKey,horKey, lookSpeed);
             HorizontalLean(player, horKey, 50, .1f);
         } 
+        */
 
         if (Input.GetKeyDown(KeyCode.Escape)) SceneManager.LoadScene("Menu", LoadSceneMode.Single);
 
@@ -42,6 +54,13 @@ public class PlayerMovement : MonoBehaviour
     void AxisMove(float x, float y, float speed){
         transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
         PositionFrame();
+        aimTarget.localPosition += new Vector3(x*3, y*3, 0) * speed * Time.deltaTime;
+        Vector3 limits = aimTarget.localPosition;
+        if (limits.x > 45) limits.x = 45;
+        if (limits.x < -45) limits.x = -45;
+        if (limits.y > 25) limits.y = 25;
+        if (limits.y < -25) limits.y = -25;
+        aimTarget.localPosition = limits;
     }
 
     void PositionFrame()
@@ -52,10 +71,26 @@ public class PlayerMovement : MonoBehaviour
         transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 
+    void RotationLook(float h, float v, float speed)
+    {   
+        
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * speed);
+        //aimTarget.localPosition = new Vector3(h, v, 10);
+        
+    }
+
      void HorizontalLean(Transform target, float axis, float leanLimit, float lerpTime)
     {
         Vector3 targetEulerAngels = target.localEulerAngles;
         target.localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -axis * leanLimit, lerpTime));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(aimTarget.position, .5f);
+        Gizmos.DrawSphere(aimTarget.position, .15f);
+
     }
 
     public void QuickSpin(int dir)
